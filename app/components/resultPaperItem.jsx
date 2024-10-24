36550433334b043722f87fe353ca67f92e62c1c3
Alfred Cheung <alfredzh2@outlook.com>
2024-10-25 01:48:50 +0800

    optimize icon image with svg file

--- app/components/resultPaperItem.jsx
+++ app/components/resultPaperItem.jsx
@@ -6,7 +6,17 @@ import Icon, {
   UpOutlined,
 } from '@ant-design/icons';
 import { Modal, Skeleton, Tooltip } from 'antd';
+import Image from 'next/image';
 import { useMemo, useState } from 'react';
+import AbstractIcon from '../img/abstractIcon.svg';
+import AchievementSimiliarIcon from '../img/achievementSimiliarIcon.svg';
+import ArticleIcon from '../img/articleIcon.svg';
+import CheckIcon from '../img/checkIcon.svg';
+import DownloadIcon from '../img/downloadIcon.svg';
+import FundSimiliarIcon from '../img/fundSimiliarIcon.svg';
+import GuideIcon from '../img/guideIcon.svg';
+import QuoteIcon from '../img/quoteIcon.svg';
+import SimiliarIcon from '../img/similiarIcon.svg';
 import { searchActor } from '../models/searchMachine';
 import {
   fetchAbstractAndTranslation as fetchAbstractAndTranslationAsync,
@@ -17,254 +27,6 @@ import {
 import CitationText from './citationText.js';
 import styles from './resultPaperItem.module.scss';
 import ResultPaperSubInfo from './resultPaperSubInfo';
-
-const AchievementSimiliarIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-  >
-    <rect x="4" y="4.00012" width="9" height="12" fill="#DEB35B" />
-    <rect x="6" y="2.00012" width="9" height="12" rx="0.1" fill="#FEEB80" />
-  </svg>
-);
-
-const FundSimiliarIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-  >
-    <path
-      d="M2.69995 15.0001C2.69995 15.2763 2.92381 15.5001 3.19995 15.5001H14.8C15.0761 15.5001 15.3 15.2763 15.3 15.0001V11.5001H2.69995V15.0001Z"
-      fill="#00A7EA"
-    />
-    <path
-      d="M4.89036 4.35098C4.95559 4.14225 5.1489 4.00012 5.3676 4.00012H12.6324C12.8511 4.00012 13.0444 4.14225 13.1096 4.35098L15.2971 11.351C15.3978 11.6729 15.1572 12.0001 14.8199 12.0001H3.1801C2.84278 12.0001 2.60224 11.6729 2.70286 11.351L4.89036 4.35098Z"
-      fill="#99DCF7"
-    />
-    <rect x="6" y="6.00012" width="6" height="1" rx="0.5" fill="white" />
-    <rect x="5" y="8.50012" width="8" height="1" rx="0.5" fill="white" />
-  </svg>
-);
-
-const SimiliarIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <circle cx="5" cy="6" r="2.5" stroke="#FFCF6D" />
-    <circle cx="9" cy="13" r="1.5" stroke="#FFCF6D" />
-    <circle cx="13.5" cy="6.5" r="2" stroke="#FFCF6D" />
-    <rect
-      opacity="0.6"
-      x="7.06995"
-      y="6"
-      width="4.37062"
-      height="1"
-      transform="rotate(4.01078 7.06995 6)"
-      fill="#FFCF6D"
-    />
-    <rect
-      opacity="0.6"
-      x="6.90393"
-      y="8"
-      width="4.37062"
-      height="1"
-      transform="rotate(64.6814 6.90393 8)"
-      fill="#FFCF6D"
-    />
-    <rect
-      opacity="0.6"
-      x="12.3826"
-      y="8.58899"
-      width="4.37062"
-      height="1"
-      transform="rotate(126.088 12.3826 8.58899)"
-      fill="#FFCF6D"
-    />
-  </svg>
-);
-
-const AbstractIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M4.64789 9H3L5.51091 2H7.49264L10 9H8.35211L6.53019 3.59961H6.47336L4.64789 9ZM4.5449 6.24854H8.43734V7.40381H4.5449V6.24854Z"
-      fill="#00A7EA"
-    />
-    <rect
-      opacity="0.4"
-      x="11"
-      y="6"
-      width="4"
-      height="2"
-      rx="1"
-      fill="#00A7EA"
-    />
-    <rect
-      opacity="0.4"
-      x="3"
-      y="10"
-      width="12"
-      height="2"
-      rx="1"
-      fill="#00A7EA"
-    />
-    <rect
-      opacity="0.4"
-      x="3"
-      y="13"
-      width="12"
-      height="2"
-      rx="1"
-      fill="#00A7EA"
-    />
-  </svg>
-);
-
-const GuideIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M6.5 5L8.00189 8.33123L11 10L8.00189 11.6688L6.5 15L4.99811 11.6688L2 10L4.99811 8.33123L6.5 5Z"
-      fill="#64DBBD"
-    />
-    <path
-      opacity="0.6"
-      d="M11 2L12.0013 4.33186L14 5.5L12.0013 6.66814L11 9L9.99874 6.66814L8 5.5L9.99874 4.33186L11 2Z"
-      fill="#64DBBD"
-    />
-    <path
-      opacity="0.6"
-      d="M13 9L14.0013 11.3319L16 12.5L14.0013 13.6681L13 16L11.9987 13.6681L10 12.5L11.9987 11.3319L13 9Z"
-      fill="#64DBBD"
-    />
-  </svg>
-);
-
-const ArticleIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M13 11V14C13 14.5523 12.553 15 12.0007 15C10.3159 15 6.81832 15 3.99894 15C3.44666 15 3 14.5523 3 14V6C3 5.44772 3.44772 5 4 5H7"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-    />
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M14.4062 3.14853C14.7169 3.14853 14.9724 3.38463 15.0031 3.68718C15.0052 3.70735 15.0062 3.72782 15.0062 3.74853L15.0062 8.54853C15.0062 8.8799 14.7376 9.14853 14.4062 9.14853C14.0749 9.14853 13.8062 8.8799 13.8062 8.54853V5.19705L7.17602 11.8273C6.94171 12.0616 6.56181 12.0616 6.32749 11.8273C6.09318 11.5929 6.09318 11.213 6.32749 10.9787L12.9577 4.34853H9.60623C9.27485 4.34853 9.00623 4.0799 9.00623 3.74853C9.00623 3.41716 9.27485 3.14853 9.60623 3.14853L14.4062 3.14853Z"
-      fill="black"
-    />
-  </svg>
-);
-
-const DownloadIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M3 12V13.5C3 14.0523 3.44684 14.5 3.99913 14.5C7.53093 14.5 12.0331 14.5 14.0024 14.5C14.5546 14.5 15 14.0523 15 13.5V12"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-    />
-    <path
-      d="M9.6 4C9.6 3.66863 9.33137 3.4 9 3.4C8.66863 3.4 8.4 3.66863 8.4 4L9.6 4ZM8.57574 12.4243C8.81005 12.6586 9.18995 12.6586 9.42426 12.4243L13.2426 8.60589C13.477 8.37157 13.477 7.99167 13.2426 7.75736C13.0083 7.52304 12.6284 7.52304 12.3941 7.75736L9 11.1515L5.60589 7.75736C5.37157 7.52304 4.99167 7.52304 4.75736 7.75736C4.52304 7.99167 4.52304 8.37157 4.75736 8.60589L8.57574 12.4243ZM8.4 4L8.4 12L9.6 12L9.6 4L8.4 4Z"
-      fill="black"
-    />
-  </svg>
-);
-
-const QuoteIcon = () => (
-  <svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect
-      x="2.33334"
-      y="5"
-      width="5.33333"
-      height="5.33333"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M7.66669 10.3334V10.5279C7.66669 12.043 6.81068 13.428 5.45554 14.1056L5.00002 14.3334"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <rect
-      x="10.3333"
-      y="5"
-      width="5.33333"
-      height="5.33333"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M15.6667 10.3334V10.5279C15.6667 12.043 14.8107 13.428 13.4555 14.1056L13 14.3334"
-      stroke="black"
-      stroke-width="1.2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
-);
-
-const CheckIcon = () => (
-  <svg
-    width="12"
-    height="12"
-    viewBox="0 0 12 12"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M1.33333 0.000244141H10.6667C11.4 0.000244141 12 0.600244 12 1.33358V10.6669C12 11.4002 11.4 12.0002 10.6667 12.0002H1.33333C0.6 12.0002 0 11.4002 0 10.6669V1.33358C0 0.600244 0.6 0.000244141 1.33333 0.000244141ZM9.06667 3.06691L10 4.00024L4.66667 9.33358L2 6.66691L2.93333 5.73358L4.66667 7.46691L9.06667 3.06691Z"
-      fill="#1D192B"
-    />
-  </svg>
-);
 
 export default function ResultPaperItem(props) {
   const {
@@ -497,7 +259,7 @@ export default function ResultPaperItem(props) {
                   toggleFundSimiliarContent(props.data);
                 }}
               >
-                <Icon component={FundSimiliarIcon} />
+                <Image src={FundSimiliarIcon} alt="" />
                 相似课题
                 {isFundSimiliarLoading && <LoadingOutlined />}
                 {!isFundSimiliarLoading &&
@@ -520,7 +282,7 @@ export default function ResultPaperItem(props) {
                   toggleAchievementSimiliarContent(props.data);
                 }}
               >
-                <Icon component={AchievementSimiliarIcon} />
+                <Image src={AchievementSimiliarIcon} alt="" />
                 相关成果
                 {isAchievementSimiliarLoading && <LoadingOutlined />}
                 {!isAchievementSimiliarLoading &&
@@ -547,7 +309,7 @@ export default function ResultPaperItem(props) {
                   toggleSimiliarContent(props.data);
                 }}
               >
-                <Icon component={SimiliarIcon} />
+                <Image src={SimiliarIcon} alt="" />
                 相似文献
                 {isSimiliarLoading && <LoadingOutlined />}
                 {!isSimiliarLoading && contentStatus === 'similiar' && (
@@ -568,7 +330,7 @@ export default function ResultPaperItem(props) {
                 }}
                 data-umami-event="abstract button"
               >
-                <Icon component={AbstractIcon} />
+                <Image src={AbstractIcon} alt="" />
                 查看摘要
                 {isAbstractLoading && <LoadingOutlined />}
                 {!isAbstractLoading && contentStatus === 'abstract' && (
@@ -589,7 +351,7 @@ export default function ResultPaperItem(props) {
                 }}
                 data-umami-event="guide button"
               >
-                <Icon component={GuideIcon} />
+                <Image src={GuideIcon} alt="" />
                 查看导读
                 {isGuideLoading && <LoadingOutlined />}
                 {!isGuideLoading && contentStatus === 'guide' && (
@@ -645,7 +407,7 @@ export default function ResultPaperItem(props) {
               </>
             ) : (
               <>
-                <Icon component={ArticleIcon} />
+                <Image src={ArticleIcon} alt="" />
                 {dataType === 'fund_cn' ? '跳转官网' : '跳转原文'}
               </>
             )}
@@ -658,7 +420,7 @@ export default function ResultPaperItem(props) {
               }}
               data-umami-event="ref button"
             >
-              <Icon component={QuoteIcon} />
+              <Image src={QuoteIcon} alt="" />
               引用
             </button>
           )}
@@ -792,9 +554,10 @@ export default function ResultPaperItem(props) {
                 <div className={styles.content_card_abstract_switch}>
                   <label>
                     {isEnAbstractVisible && (
-                      <Icon
+                      <Image
                         className={styles.content_card_abstract_switch_icon}
-                        component={CheckIcon}
+                        src={CheckIcon}
+                        alt=""
                       />
                     )}
                     <input
